710ded3773cb2e079a4f9f9cbbb70b99b7186093
Barry1003 <isholausaamah@gmail.com>
2025-07-05 13:43:30 +0100

    image fixed

--- components/nav.tsx
+++ components/nav.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className='w-full p-4 flex justify-between items-center  text-white'>
       <div className="div">
-        <img src="../Public/logo.png" alt="website logo" />
+        <img src="/logo.png" alt="website logo" />
       </div>
       <div className="rounded-full">
         <h1 className="text-xl font-semibold text-white rounded-full px-6 py-2 bg-[#2BF388]">Join Free</h1>
